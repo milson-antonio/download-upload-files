1c7202daaea128c5ab66919c750f9a5e9ec54653
milson.antonio <milson.milson@outlook.com>
2024-04-01 18:35:17 +0200

    added missing files

---
+++ src/main/kotlin/com/milsondev/downloaduploadfiles/DownloadUploadFilesApplication.kt
@@ -0,0 +1,11 @@
+package com.milsondev.downloaduploadfiles
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DownloadUploadFilesApplication
+
+fun main(args: Array<String>) {
+	runApplication<DownloadUploadFilesApplication>(*args)
+}
